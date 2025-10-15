cf402a9d8c6e4d482e24042283b77d3a4a036b22
qmet1324 <mikhail.melnik.gamedev@gmail.com>
2025-10-15 15:35:19 -0400

    feat: set up the basic layout of the app

--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
 #include "mainwindow.h"
-
 #include <QApplication>
 
-int main(int argc, char *argv[])
-{
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-    return a.exec();
+int main(int argc, char *argv[]) {
+  QApplication a(argc, argv); // Init application
+
+  // Create the UI
+  MainWindow window;
+  window.resize(800, 600);
+  window.show();
+
+  return a.exec(); // Execute eventloop
 }
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,48 @@
 #include "mainwindow.h"
-#include "./ui_mainwindow.h"
+#include <QGroupBox>
+#include <QHBoxLayout>
+#include <QLabel>
+#include <QListWidget>
+#include <QPushButton>
+#include <QTextEdit>
+#include <QVBoxLayout>
 
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
-{
-    ui->setupUi(this);
+MainWindow::MainWindow(QWidget *parent) : QWidget(parent) {
+  auto toDoColumn = new QGroupBox("To-Do");
+  auto toDoList = new QListWidget;
+
+  auto inProgressColumn = new QGroupBox("In Progress");
+  auto inProgressList = new QListWidget;
+
+  auto doneColumn = new QGroupBox("Done");
+  auto doneList = new QListWidget;
+
+  auto addTaskButton = new QPushButton("+");
+
+  // Create To-Do column
+  auto toDoColumnLayout = new QVBoxLayout;
+  toDoColumnLayout->addWidget(addTaskButton);
+  toDoColumnLayout->addWidget(toDoList);
+  toDoColumn->setLayout(toDoColumnLayout);
+  toDoColumn->setAlignment(Qt::AlignCenter);
+
+  // Create In Progress Column
+  auto inProgressColumnLayout = new QVBoxLayout;
+  inProgressColumnLayout->addWidget(inProgressList);
+  inProgressColumn->setLayout(inProgressColumnLayout);
+  inProgressColumn->setAlignment(Qt::AlignCenter);
+
+  // Create Done Column
+  auto doneColumnLayout = new QVBoxLayout;
+  doneColumnLayout->addWidget(doneList);
+  doneColumn->setLayout(doneColumnLayout);
+  doneColumn->setAlignment(Qt::AlignCenter);
+
+  // Set Horizontal Layout for all columns
+  auto mainLayout = new QHBoxLayout(this);
+  mainLayout->addWidget(toDoColumn);
+  mainLayout->addWidget(inProgressColumn);
+  mainLayout->addWidget(doneColumn);
 }
 
-MainWindow::~MainWindow()
-{
-    delete ui;
-}
+MainWindow::~MainWindow() {}
